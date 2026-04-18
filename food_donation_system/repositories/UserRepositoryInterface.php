c7e1da9f52bc553526521fb60ed0974ea85a3b57
Aysha Siddika <ayshatithi510@gmail.com>
2026-04-18 11:23:38 +0600

    Delete food_donation_system directory

--- food_donation_system/repositories/UserRepositoryInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-interface UserRepositoryInterface
-{
-    public function findByEmail(string $email): ?User;
-    public function findById(int $id): ?User;
-    public function create(string $name, string $email, string $passwordHash, string $role): bool;
-}
